9f46a9034b2be8b646b487579cc562e0bd982a2d
montaghanmy <monta.ghanmy@gmail.com>
2023-02-07 15:03:20 +0100

    feat: added mul

---
+++ __tests__/feature.test.js
@@ -0,0 +1,9 @@
+const mul = require('../feature');
+
+test('multiply 1 * 2 to equal 2', () => {
+  expect(mul(1, 2)).toBe(2);
+});
+
+test('multiply 2 * 2 to equal 4', () => {
+  expect(mul(2, 2)).toBe(4);
+});
