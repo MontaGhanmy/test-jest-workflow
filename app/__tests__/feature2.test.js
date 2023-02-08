8a617f587a484ca46fbcb341ee0a99d5b9509184
montaghanmy <monta.ghanmy@gmail.com>
2023-02-08 15:50:01 +0100

    feat: updated test

--- app/__tests__/feature2.test.js
+++ app/__tests__/feature2.test.js
@@ -3,3 +3,7 @@ const sub = require('../feature2');
 test('sub 2 * 2 to equal 0', () => {
   expect(sub(2, 2)).toBe(0);
 });
+
+test('sub 2 * 1 to equal 0', () => {
+  expect(sub(2, 2)).toBe(1);
+});
